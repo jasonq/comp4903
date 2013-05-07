a9144e436999b343afe5c2cc9efb507e7e6e236b
Jason Quan <jason@appnovation.com>
2013-05-07 12:46:35 -0700

    added unit movebox method

---
+++ Project/src/com/comp4903/pathfind/PathFind.java
@@ -0,0 +1,29 @@
+package com.comp4903.pathfind;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.graphics.Point;
+
+import com.comp4903.project.gameEngine.data.MapData;
+import com.comp4903.project.gameEngine.data.Unit;
+import com.comp4903.project.gameEngine.factory.GameStats;
+
+public class PathFind {
+
+	private static MapData _map;
+	public static void initialize(MapData map) { _map = map; Algorithms.initialize(map); }
+	
+	public void getMovePoints(Unit u){
+		List<Point> l = Algorithms.GetPointsBFS(u.position, u.combatStats.maxMovement);
+		for(Unit unit : _map._units){
+			for(Point p : l){
+				if(unit.position.equals(p))
+					l.remove(p);
+			}
+		}
+		_map.clearBoxes();
+		
+		_map._movementBox = l;
+	}
+}
