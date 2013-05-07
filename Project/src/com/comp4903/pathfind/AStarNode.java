b0e677e5fe9bcda5551cc164d83c91fa6b1933c7
Jason Quan <jason@appnovation.com>
2013-05-07 11:37:49 -0700

    pathfind update

--- Project/src/com/comp4903/pathfind/AStarNode.java
+++ Project/src/com/comp4903/pathfind/AStarNode.java
@@ -4,12 +4,35 @@ import com.comp4903.project.gameEngine.data.Point;
 
 public class AStarNode
 {
-    public Point p;
-    public AStarNode parent;
-    public int g;
-    public int f;
+    private Point p;
+    private AStarNode parent;
+    private int g;
+    private int f;
+    
+    /**
+     * Constructor
+     * @param p point
+     * @param parent parent node
+     * @param g value to determine length of path
+     * @param f value to determine which path to take to get to end
+     */
     public AStarNode(Point p, AStarNode parent, int g, int f)
     {
         this.p = p; this.parent = parent; this.g = g; this.f = f;
     }
+    
+    /************************/
+    /**   Getter Methods   **/
+    /************************/
+    public Point getP() { return p; }
+    public AStarNode getParent() { return parent; }
+    public int getG() { return g; }
+    public int getF() { return f; }
+    /************************/
+    /**   Setter Methods   **/
+    /************************/
+    public void setP(Point p) { this.p = p; }
+    public void setParent(AStarNode p) { parent = p; }
+    public void setG(int i) { g = i; }
+    public void setF(int i) { f = i; }
 }
